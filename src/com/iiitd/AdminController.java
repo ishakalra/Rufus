f46902884f5a3fac6f22590f0c03d574e6dc0e6d
mansigoel <mansi14062@iiitd.ac.in>
2016-03-25 00:48:36 +0530

    Controller classes added

---
+++ src/com/iiitd/AdminController.java
@@ -0,0 +1,199 @@
+package com.iiitd;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class AdminController {
+
+    @FXML
+    private DatePicker csince;
+
+    @FXML
+    private ComboBox<?> cplayer;
+
+    @FXML
+    private TextField rname;
+
+    @FXML
+    private Button padd;
+
+    @FXML
+    private TextField cname;
+
+    @FXML
+    private ComboBox<?> ppro;
+
+    @FXML
+    private ComboBox<?> mname;
+
+    @FXML
+    private ComboBox<?> tcourt;
+
+    @FXML
+    private Button logout;
+
+    @FXML
+    private ComboBox<?> myear;
+
+    @FXML
+    private Button radd1;
+
+    @FXML
+    private ComboBox<?> mplost;
+
+    @FXML
+    private DatePicker cdob;
+
+    @FXML
+    private Button radd;
+
+    @FXML
+    private Button tadd;
+
+    @FXML
+    private ComboBox<?> mreferee;
+
+    @FXML
+    private Button sadd1;
+
+    @FXML
+    private TextField pname;
+
+    @FXML
+    private Button madd;
+
+    @FXML
+    private ComboBox<?> mpwon;
+
+    @FXML
+    private TextField tname;
+
+    @FXML
+    private ComboBox<?> tyear;
+
+    @FXML
+    private Button madd1;
+
+    @FXML
+    private Button cadd;
+
+    @FXML
+    private DatePicker rdob;
+
+    @FXML
+    private DatePicker ctill;
+
+    @FXML
+    private DatePicker pdob;
+
+    @FXML
+    private Button cadd1;
+
+    @FXML
+    private Button tadd1;
+
+    @FXML
+    private ComboBox<?> ppoints;
+
+    @FXML
+    private TextField sname;
+
+    @FXML
+    private Button padd1;
+
+    @FXML
+    private ComboBox<?> tcategory;
+
+    @FXML
+    private ComboBox<?> ccountry;
+
+    @FXML
+    private ComboBox<?> rcountry;
+
+    @FXML
+    private ComboBox<?> mround;
+
+    @FXML
+    private Button sadd;
+
+    @FXML
+    private ComboBox<?> pcountry;
+
+    @FXML
+    private ComboBox<?> syear;
+
+    @FXML
+    private ComboBox<?> splayer;
+
+    @FXML
+    void lout(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addp(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addp1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addm(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addm1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addc(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addc1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addr(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addr1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void adds(ActionEvent event) {
+
+    }
+
+    @FXML
+    void adds1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addt1(ActionEvent event) {
+
+    }
+
+}
+
---
+++ src/com/iiitd/WelcomeController.java
@@ -0,0 +1,74 @@
+package com.iiitd;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class WelcomeController {
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private Button sinfo;
+
+    @FXML
+    private Button cinfo;
+
+    @FXML
+    private Button pinfo;
+
+    @FXML
+    private Button rinfo;
+
+    @FXML
+    private Button mstats;
+
+    @FXML
+    private Button login;
+
+    @FXML
+    private Button hth;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    void headtohead(ActionEvent event) {
+
+    }
+
+    @FXML
+    void matchstats(ActionEvent event) {
+
+    }
+
+    @FXML
+    void coachinfo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sponsorinfo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void refereeinfo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void playerinfo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void lin(ActionEvent event) {
+
+    }
+
+}
