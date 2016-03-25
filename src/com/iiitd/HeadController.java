a182dce3a986f8731e37ee3230220a3be69eef91
mansigoel <mansi14062@iiitd.ac.in>
2016-03-25 23:54:49 +0530

    completed player and hth gui

--- src/com/iiitd/HeadController.java
+++ src/com/iiitd/HeadController.java
@@ -1,5 +1,76 @@
 package com.iiitd;
 
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+
 public class HeadController {
 
+    @FXML
+    private Label match2;
+
+    @FXML
+    private Label match1;
+
+    @FXML
+    private Label country1;
+
+    @FXML
+    private Label title1;
+
+    @FXML
+    private Button backb;
+
+    @FXML
+    private Label title2;
+
+    @FXML
+    private PieChart pie;
+
+    @FXML
+    private Label country2;
+
+    @FXML
+    private Button filter;
+
+    @FXML
+    private Label dob2;
+
+    @FXML
+    private Label dob1;
+
+    @FXML
+    private Label atp2;
+
+    @FXML
+    private Label atp1;
+
+    @FXML
+    private Label head1;
+
+    @FXML
+    private Label head2;
+
+    @FXML
+    private ComboBox<?> name2;
+
+    @FXML
+    private ComboBox<?> name1;
+
+    @FXML
+    void apply(ActionEvent event) {
+
+    }
+
+    @FXML
+    void back(ActionEvent event) {
+
+    }
+
 }
+
--- src/com/iiitd/PlayerController.java
+++ src/com/iiitd/PlayerController.java
@@ -1,5 +1,87 @@
 package com.iiitd;
 
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
 public class PlayerController {
+
+    @FXML
+    private TextField match2;
+
+    @FXML
+    private ComboBox<?> country;
+
+    @FXML
+    private TextField match1;
+
+    @FXML
+    private TableColumn<?, ?> cpro;
+
+    @FXML
+    private TableColumn<?, ?> cname;
+
+    @FXML
+    private ComboBox<?> wont;
+
+    @FXML
+    private Button back;
+
+    @FXML
+    private ComboBox<?> court;
+
+    @FXML
+    private ComboBox<?> pro1;
+
+    @FXML
+    private DatePicker born2;
+
+    @FXML
+    private Button filter;
+
+    @FXML
+    private ComboBox<?> pro2;
+
+    @FXML
+    private DatePicker born1;
+
+    @FXML
+    private TableColumn<?, ?> catp;
+
+    @FXML
+    private TableColumn<?, ?> ctitle;
+
+    @FXML
+    private ComboBox<?> name;
+
+    @FXML
+    private TableColumn<?, ?> ccountry;
+
+    @FXML
+    private TextField atp2;
+
+    @FXML
+    private TextField atp1;
+
+    @FXML
+    private TableColumn<?, ?> cdob;
+
+    @FXML
+    private TableColumn<?, ?> cmatch;
+
+    @FXML
+    void apply(ActionEvent event) {
+
+    }
+
+    @FXML
+    void backb(ActionEvent event) {
+
+    }
 
 }
