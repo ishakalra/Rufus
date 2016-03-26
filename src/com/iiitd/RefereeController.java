18b62a5a18d9f1cfe50726e269f7ec3795fa63da
mansigoel <mansi14062@iiitd.ac.in>
2016-03-26 23:27:36 +0530

    sponsor and referee gui added

--- src/com/iiitd/RefereeController.java
+++ src/com/iiitd/RefereeController.java
@@ -1,5 +1,77 @@
 package com.iiitd;
 
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class RefereeController {
 
+    @FXML
+    private ComboBox<?> country;
+
+    @FXML
+    private TableColumn<?, ?> cmatches;
+
+    @FXML
+    private ComboBox<?> player1;
+
+    @FXML
+    private Button apply;
+
+    @FXML
+    private ComboBox<?> player2;
+
+    @FXML
+    private TableColumn<?, ?> cname;
+
+    @FXML
+    private Button back;
+
+    @FXML
+    private ComboBox<?> tournament;
+
+    @FXML
+    private ComboBox<?> court;
+
+    @FXML
+    private ComboBox<?> year1;
+
+    @FXML
+    private ComboBox<?> year2;
+
+    @FXML
+    private TableView<?> refereetable;
+
+    @FXML
+    private DatePicker dob2;
+
+    @FXML
+    private DatePicker dob1;
+
+    @FXML
+    private ComboBox<?> name;
+
+    @FXML
+    private TableColumn<?, ?> ccountry;
+
+    @FXML
+    private TableColumn<?, ?> cdob;
+
+    @FXML
+    void backb(ActionEvent event) {
+
+    }
+
+    @FXML
+    void filter(ActionEvent event) {
+
+    }
+
 }
+
--- src/com/iiitd/SponsorController.java
+++ src/com/iiitd/SponsorController.java
@@ -1,5 +1,81 @@
 package com.iiitd;
 
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class SponsorController {
 
+    @FXML
+    private ComboBox<?> country;
+
+    @FXML
+    private TableColumn<?, ?> cplayer;
+
+    @FXML
+    private Button apply;
+
+    @FXML
+    private TableColumn<?, ?> cname;
+
+    @FXML
+    private Button back;
+
+    @FXML
+    private ComboBox<?> title1;
+
+    @FXML
+    private ComboBox<?> title2;
+
+    @FXML
+    private ComboBox<?> tournament;
+
+    @FXML
+    private TableColumn<?, ?> cyear;
+
+    @FXML
+    private ComboBox<?> year1;
+
+    @FXML
+    private ComboBox<?> year2;
+
+    @FXML
+    private TableColumn<?, ?> catp;
+
+    @FXML
+    private TableColumn<?, ?> ctitle;
+
+    @FXML
+    private ComboBox<?> name;
+
+    @FXML
+    private ComboBox<?> atp2;
+
+    @FXML
+    private TableColumn<?, ?> ccountry;
+
+    @FXML
+    private ComboBox<?> atp1;
+
+    @FXML
+    private TableView<?> sponsortable;
+
+    @FXML
+    private ComboBox<?> player;
+
+    @FXML
+    void filter(ActionEvent event) {
+
+    }
+
+    @FXML
+    void backb(ActionEvent event) {
+
+    }
+
 }
+
